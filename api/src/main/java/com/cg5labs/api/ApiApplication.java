c3ecd24e6ffc6fbc8b63aea6c069f03f620a42c2
cg5labs <cgscheible@yahoo.com>
2023-09-05 19:29:40 +0800

    api/pom.xml - add war-build

--- api/src/main/java/com/cg5labs/api/ApiApplication.java
+++ api/src/main/java/com/cg5labs/api/ApiApplication.java
@@ -2,9 +2,11 @@ package com.cg5labs.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
